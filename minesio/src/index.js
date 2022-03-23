21416f4d634ed9c6b02c46e9b1c1f1f1de1a7d4a
marceau-m <marceau.mathieu@depinfonancy.net>
2022-03-23 11:33:46 +0100

    First version interface minesio

--- minesio/src/index.js
+++ minesio/src/index.js
@@ -2,83 +2,176 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {    
-        super(props);    
-        this.state = {
-            value: null,   
-        };  
+let opponents= {
+  "players": [
+    {
+      "name": "Vaxou",
+      "position": (100,200),
+      "size":100
+     },
+    {
+      "name": "Rousseau",
+      "position":(500,300),
+      "size": 200
     }
-    render() {
-      return (
-        <button
-            className="square"        
-            onClick={() => this.setState({value: 'X'})}     
-        >
-            {this.state.value}
-        </button>
-      );
-    }
+  ]
+}
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="player">
+          <Circle />
+        </div>
+        <div className="opponents">
+          <Opponent/>
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
   }
-  
-  class Board extends React.Component {
-    constructor(props) {    
-        super(props);    
-        this.state = {      
-            squares: Array(9).fill(null),    
-        };
-    }
+}
 
-    renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
+class Circle extends React.Component {
+  render() { 
+    return (
+      <div class="circle">
+        <img src={require('./images/Ciarletta.png')} alt="Ciarletta"/>
+        <p class="text">JohnDu54</p>
+      </div>
+    )
   }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
+}
+
+class Opponent extends React.Component {
+  render() {
+    return (
+      <div class="opponent" position="absolute">
+          <img src={require('./images/Vaxou.png')} alt="Mechant"/>
+          <p class="text"> Vaxou</p>
+      </div>
+    )
   }
+}
+// =================================================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
+// function Square(props) {
+//   return (
+//     <button className="square" onClick={props.onClick}>
+//       {props.value}
+//     </button>
+//   );
+// }
   
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+// class Board extends React.Component {
+//   constructor(props) {    
+//       super(props);    
+//       this.state = {      
+//           squares: Array(9).fill(null),
+//           xIsNext:true,     
+//       };
+//   }
+
+//   handleClick(i) {    
+//     const squares = this.state.squares.slice();
+//     if (calculateWinner(squares) || squares[i]) {      
+//       return;    
+//     } 
+//     squares[i] = this.state.xIsNext ? 'X' : 'O';
+//     this.setState({
+//       squares:squares,
+//       xIsNext: !this.state.xIsNext,
+//     });  
+//   }
+
+//   renderSquare(i) {
+//     return (
+//       <Square
+//         value={this.state.squares[i]}
+//         onClick={() => this.handleClick(i)}      />
+//     );
+//   }
+
+//   render() {
+//     const winner = calculateWinner(this.state.squares);
+//     let status;
+//     if (winner) {      
+//       status = winner + ' a gagné';    
+//     } else {      
+//       status = 'Prochain joueur : ' + (this.state.xIsNext ? 'X' : 'O');    
+//     }
+
+//     return (
+//       <div>
+//         <div className="status">{status}</div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}
+//           {this.renderSquare(1)}
+//           {this.renderSquare(2)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(3)}
+//           {this.renderSquare(4)}
+//           {this.renderSquare(5)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(6)}
+//           {this.renderSquare(7)}
+//           {this.renderSquare(8)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// class Game extends React.Component {
+//   render() {
+//     return (
+//       <div className="game">
+//         <div className="game-board">
+//           <Board />
+//         </div>
+//         <div className="game-info">
+//           <div>{/* status */}</div>
+//           <ol>{/* TODO */}</ol>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// // ========================================
+
+// ReactDOM.render(
+//   <Game />,
+//   document.getElementById('root')
+// );
+
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
