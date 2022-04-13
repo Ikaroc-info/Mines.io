c83e90fe090efa318e909bae573f05ecf7d47a05
marceau-m <marceau.mathieu@depinfonancy.net>
2022-04-13 11:46:02 +0200

    finish interface play

--- minesio/src/index.js
+++ minesio/src/index.js
@@ -18,28 +18,41 @@ let players= {
     "size":300,
     "image":"Vaxou.png",
   }
-}
+};
 
 let foods = {
   "food1": [350,500],
   "food2": [200,100],
   "food3": [175,400],
   "food4": [900,230],
-  "food5": [977,500]}
-let foodsize = 100 
+  "food5": [977,500]};
+let foodsize = 100; 
 
-let playersize=200
+let playersize=200;
 let w = window.innerWidth;
 let h = window.innerHeight;
-let myname= "JohnDu54"
-
+let myname= "JohnDu54";
 
 
 class Game extends React.Component {
+  _onMouseMove(e) {
+    this.setState({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
+  }
+  constructor(props) {
+    super(props);
+    this.state = { x: 0, y: 0 };
+  }
   render() {
+    const { x, y } = this.state;
+    var x_vect=x-w/2
+    var y_vect=y-h/2
+    var norm = Math.sqrt(x_vect*x_vect+y_vect*y_vect)
+    x_vect=x_vect/norm
+    y_vect=-y_vect/norm
+    console.log(x_vect,y_vect)
     return (
-      <div className="parent">
-        <div className="game">
+      <div className="parent" onMouseMove={this._onMouseMove.bind(this)}>
+        <div id="game">
           <div className="opponents">
               <Opponents/>
           </div>
@@ -51,6 +64,7 @@ class Game extends React.Component {
         <div className="food">
           <Food/>
         </div>
+        <div id="mouseInfo" width="400px" height="400px"></div>
       </div>
     );
   }
@@ -71,7 +85,7 @@ class Opponents extends React.Component {
       top: h/2 + players[player]["position"][1] - players[myname]["position"][1]- 200*scale/2, 
       width: playersize*scale, 
       height: playersize*scale,
-    }
+    };
     return (
       <div className="opponent" style={playerStyle}>
         <img src={require('./images/'+players[player]["image"])} alt="Mechant"/>
@@ -104,20 +118,20 @@ class Food extends React.Component {
       width: foodsize*scale, 
       height: foodsize*scale,
       background_color: "blue",
-    }
+    };
     return (
       <div className="food" style={foodStyle}>
         <img src={require('./images/'+'Ciarletta.png')} alt="Mechant"/>
       </div>
     )
-  }
+  };
   render() {
     return (
       <div className="fooddisplay">
         {Object.keys(foods).map(this.renderFood)}
       </div>
     );
-  }
+  };
 }
 
 // =================================================================
